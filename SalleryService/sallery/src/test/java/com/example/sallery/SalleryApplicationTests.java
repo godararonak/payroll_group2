d99542c574f91144e493ba3370cc8b6f6d57f80d
Rajdeep Nagar <rajdeep.nagar@ukg.com>
2024-08-08 13:37:49 +0530

    first commit

---
+++ SalleryService/sallery/src/test/java/com/example/sallery/SalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
