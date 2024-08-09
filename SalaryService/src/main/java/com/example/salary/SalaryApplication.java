8b0192ef96996cda7382a35e63fc343ab64b0482
Aghnash Ranjith <aghnash.ranjith@ukg.com>
2024-08-09 10:25:16 +0530

    salary leaves entity, renamed files and folders

--- SalaryService/src/main/java/com/example/salary/SalaryApplication.java
+++ SalaryService/src/main/java/com/example/salary/SalaryApplication.java
@@ -1,13 +1,13 @@
-package com.example.sallery;
+package com.example.salary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SalleryApplication {
+public class SalaryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SalleryApplication.class, args);
+		SpringApplication.run(SalaryApplication.class, args);
 	}
 
 }
---
+++ SalaryService/src/main/java/com/example/salary/entity/leave.java
@@ -0,0 +1,42 @@
+package com.example.salary.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Entity(name = "leaves")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class leave {
+    //TODO Are enums necessary?
+    public enum LeaveType {
+        PAID,
+        UNPAID
+    }
+
+    public enum LeaveStatus {
+        PENDING,
+        APPROVED,
+        REJECTED
+    }
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+    private long id;
+    private LeaveType leaveType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private long numOfDays;
+    private LeaveStatus leaveStatus;
+
+}
