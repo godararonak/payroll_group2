2f990303bf22bee5ae3f602e471b2d6149c6e0ce
Rajdeep Nagar <rajdeep.nagar@ukg.com>
2024-08-20 21:19:40 +0530

    passing userId during login

---
+++ AuthServer/src/main/java/com/example/AuthServer/dto/UserDto.java
@@ -0,0 +1,38 @@
+package com.example.salary.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+
+    private String f_name;
+
+
+    private String l_name;
+
+    private LocalDate date_of_birth;
+
+    private LocalDate hire_Date;
+
+    private String email;
+
+    private String Phone_Number;
+
+    private String role;
+
+    private long Manager_Id;
+
+    private double total_ctc;
+
+    private String password;
+
+
+}
